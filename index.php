d8ba94533456b6bc485f931ead69b9ba53ae298e
Michael Calizo <mcalizo@mcalizo.remote.csb>
2017-11-28 18:30:51 +1300

    update php

--- index.php
+++ index.php
@@ -3,9 +3,8 @@ echo "Hello World! My App deployed via Ansible Tower v3.2.1";
 echo "<br>";
 echo "<br>";
 echo $_SERVER['SERVER_NAME'];
-echo "br>";
-echo "br>";
-
+echo "<br>";
+echo "<br>";
 echo "<a href='index.php'><img border='0'src='https://ibb.co/mdBotR'></a>";
 
 ?>
