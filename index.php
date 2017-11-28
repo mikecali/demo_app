d3dd3ef53ccf3e57ced107a60de61ed31deb0267
root <root@mcalizo.remote.csb>
2017-11-28 21:47:48 +1300

    fix php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "Hello World! My App deployed via Ansible Tower v3.2.1";
 echo "<br>";
 echo "<br>";
-echo "The Hostname of this page is:" $_SERVER['SERVER_NAME'];
+echo "The Hostname of this page is:"; $_SERVER['SERVER_NAME'];
 echo "<br>";
 echo "<br>";
 echo "<a href='index.php'><img src='https://image.ibb.co/m9dRDR/Screenshot_from_2017_11_28_19_13_11.png' /></a>";
