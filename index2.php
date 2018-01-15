2a4571f285f52f39491c1e920f7b9d281b0ef069
mikecali <mike.calizo@gmail.com>
2018-01-16 08:59:41 +1300

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,10 @@
-https://ibb.co/dWwv2R
+<?php
+echo "Hello World! My App deployed via Ansible Tower v3.2.1";
+echo "<br>";
+echo "<br>";
+echo "The Hostname of this page is:"; 
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo "<br>";
+echo "<a href='index.php'><img src='https://ibb.co/dWwv2R' /></a>";
+?>
